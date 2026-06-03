0d8714f13cd0ff7418868763c22a1fe3d72629af
SHuzaifaAli <3.7245187e+07+SHuzaifaAli@users.noreply.github.com>
2026-06-03 15:18:52 +0000

    Build: Generate missing platform-specific files (Android, iOS, Web)

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -3,6 +3,28 @@
 // Generated file. Do not edit.
 //
 
+// @dart = 2.13
 // ignore_for_file: type=lint
 
-void registerPlugins() {}
+import 'package:agora_rtc_engine/agora_rtc_engine_web.dart';
+import 'package:app_links_web/app_links_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_messaging_web/firebase_messaging_web.dart';
+import 'package:iris_method_channel/iris_method_channel_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AgoraRtcEngineWeb.registerWith(registrar);
+  AppLinksPluginWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseMessagingWeb.registerWith(registrar);
+  IrisMethodChannelWeb.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,24 @@
 import FlutterMacOS
 import Foundation
 
+import agora_rtc_engine
+import app_links
+import firebase_core
+import firebase_messaging
+import iris_method_channel
+import path_provider_foundation
+import shared_preferences_foundation
+import sqflite
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  IrisMethodChannelPlugin.register(with: registry.registrar(forPlugin: "IrisMethodChannelPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
